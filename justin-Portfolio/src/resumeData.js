4727cb8320acae13143b0b78868cca70b673ba97
justinPC <justinferron@me.com>
2019-09-03 08:52:57 +0200

    init commit

---
+++ justin-Portfolio/src/resumeData.js
@@ -0,0 +1,166 @@
+let resumeData = {
+    "name": "Justin Ferron",
+    "linkedinId": " justin-ferron",
+    "roleDescription": "Software Innovation Engineer, creating software for new innovations working in frontend languages like angular and react. I also let my creativity come by in my work ",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/justin-ferron/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/jushaa",
+            "className": "fa fa-github"
+        },
+    ],
+    "aboutme": "I am currently a final year student at The Hogeschool van Amsterdam. My interest was always in computers, from a young age I would play games on our\n" +
+        "                                computer. It started with gaming and after that I got interested in doing other stuff like\n" +
+        "                                making music\n" +
+        "                                in FL Studio, creating videos and photoshops in all kinds of Adobe programs. So doing\n" +
+        "                                creative stuff and then I started to code. ",
+    "address": "Amsterdam-SouthEast",
+    "emailaddress": "justinferron@me.com",
+    "mobilephone": "(036) 53 888 006",
+    "education": [
+        {
+            "UniversityName": "The Hogeschool van Amsterdam",
+            "specialization": "Software Engineering",
+            "MonthOfPassing": "Sep 2016",
+            "YearOfPassing": "till Aug 2020",
+        },
+        {
+            "UniversityName": "Luzac Amsterdam",
+            "specialization": "Havo(redo economics)",
+            "MonthOfPassing": "Sep 2015",
+            "YearOfPassing": "Till May 2016",
+        },
+        {
+            "UniversityName": "OSG de Meergronden",
+            "specialization": "Havo(economics)",
+            "MonthOfPassing": "Sep 2013",
+            "YearOfPassing": "Till May 2016",
+        },
+        {
+            "UniversityName": "OSG de Meergronden",
+            "specialization": "TL(economics)",
+            "MonthOfPassing": "Sep 2009",
+            "YearOfPassing": "Till May 2013",
+        }
+    ],
+    "work": [
+        {
+            "CompanyName": "Ahold Delhaize",
+            "specialization": "Software innovation engineer",
+            "MonthOfLeaving": "Aug 2018",
+            "YearOfLeaving": "Till now",
+        },
+        {
+            "CompanyName": "Ahold Delhaize",
+            "specialization": "Software innovation engineer intern",
+            "MonthOfLeaving": "Feb 2018",
+            "YearOfLeaving": "Till Aug 2018",
+        },
+        {
+            "CompanyName": "ICT at Huysmeester",
+            "specialization": "Creating websites for customers",
+            "MonthOfLeaving": "Dec 2017",
+            "YearOfLeaving": "Till Jan 2019",
+        },
+        {
+            "CompanyName": "Alphatronics",
+            "specialization": "Research and developement",
+            "MonthOfLeaving": "Jun 2017",
+            "YearOfLeaving": "Till Jan 2018",
+        },
+        {
+            "CompanyName": "Yonahcreations(my business)",
+            "specialization": "Webdeveloper",
+            "MonthOfLeaving": "Dec 2015",
+            "YearOfLeaving": "Till Jan 2017",
+        },
+        {
+            "CompanyName": "Snackbar Juma and Kroon",
+            "specialization": "Catering",
+            "MonthOfLeaving": "Apr 2014",
+            "YearOfLeaving": "Till Jan 2018",
+        },
+        {
+            "CompanyName": "Albert Heijn and Plus",
+            "specialization": "Stock clerk",
+            "MonthOfLeaving": "Jun 2012",
+            "YearOfLeaving": "Till Apr 2014",
+        },
+    ],
+    "skillsDescription": "My skills",
+    "skills": [
+        {
+            "skillnameCSS": "HTML5",
+            "normalSkillName": "HTML/CSS",
+            "skillPercentage" : "75%"
+
+        },
+        {
+            "skillnameCSS": "Angular",
+            "normalSkillName": "Angular",
+            "skillPercentage" : "75%"
+
+        },
+        {
+            "skillnameCSS": "Reactjs",
+            "normalSkillName": "React",
+            "skillPercentage" : "25%"
+
+        },
+        {
+            "skillnameCSS": "Java",
+            "normalSkillName": "Java",
+            "skillPercentage" : "50%"
+
+        },
+        {
+            "skillnameCSS": "Python",
+            "normalSkillName": "Python",
+            "skillPercentage" : "50%"
+
+        },
+        {
+            "skillnameCSS": "postgreSQL",
+            "normalSkillName": "PostgreSQL and MySQL",
+            "skillPercentage" : "75%"
+
+        },
+    ],
+    "portfolio": [
+        {
+            "name": "G2A Price Checker",
+            "description": "This was a basic sales checker. If the price of a product became lower then X price I would get\n" +
+                "                        a\n" +
+                "                        notification. For more information check out my GitHub Page",
+            "imgurl": "/images/g2achecker.png",
+            "linkToUrl": ""
+        },
+        {
+            "name": "Motorcycle trailer(Demo)",
+            "description": "This was a fun project I created myself. I just bought my first motorcycle and wanted to create\n" +
+                "                        a video about it. So I made a cinematic reveal demo.",
+            "imgurl": "images/motor.jpg",
+            "linkToUrl": "https://www.youtube.com/watch?v=BNr583M4yXQ"
+        },
+        {
+            "name": "Weather application(for school)",
+            "description": "For this project we needed to create a webapp(this was a school assignment that used different APIS. This is a demo of that\n" +
+                "                        project",
+            "imgurl": "images/weatherapplication.png",
+            "linkToUrl": "https://github.com/jushaa/weatherapplication"
+        },
+        {
+            "name": "Portfolio",
+            "description": "This is a small project. It is the portfolio you are looking at right now. More info on my GitHub",
+            "imgurl": "images/portfolio.png",
+            "linkToUrl": "https://github.com/jushaa/portfolio"
+        }
+    ]
+};
+
+export default resumeData
